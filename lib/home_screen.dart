dd966e1adcd117559220172712bf9ea5fd79209d
minanader11 <nadermina30@gmail.com>
2024-01-29 14:06:08 +0200

    added app bar in scaffold

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
- return Scaffold();
+ return Scaffold(appBar: AppBar(title: Text('mina') ,),);
 
   }
 }
